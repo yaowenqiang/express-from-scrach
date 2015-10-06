996ed2f74b7b4b08684d7bc34ddc935f7dfe9494
yaowenqiang <yaowenqiang111@163.com>
2015-10-06 15:53:11 +0800

    add scope demo

---
+++ scope_a.js
@@ -0,0 +1,13 @@
+var one = 1;
+function two(){
+    return 'two';
+}
+global.three = 'three';
+global.four = function(){
+     return 'four'
+}
+exports.five = 'five';
+exports.six = function(){
+     return 'dix';
+
+}
---
+++ scope_b.js
@@ -0,0 +1,10 @@
+//require('./scope_a.js');
+var moda = require('./scope_a.js');
+//console.log(one);
+//console.log(two());
+//console.log(three);
+//console.log(four());
+     //return 'six';
+//}
+console.log(moda.five);
+console.log(moda.six());
