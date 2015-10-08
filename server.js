ad4f9d4ef688d8375e282979a840fc10640e57cc
yaowenqiang <yaowenqiang111@163.com>
2015-10-09 01:44:47 +0800

    add some request properties

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var server = http.createServer(function(request,response){
     response.write('<h1>Hello,Node</h1>');
+    response.write('<p>Method:'+request.method+'</p>');
+    response.write('<p>URL:'+request.url+'</p>');
     response.end();
 });
 server.listen(3000);
+
+
