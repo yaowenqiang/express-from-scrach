ade2de03aecff7e9628d72d4ab2bb0c8eef7b4e6
yaowenqiang <yaowenqiang111@163.com>
2015-10-09 01:36:16 +0800

    add the Transform stream demo

--- stdin_stdout.js
+++ stdin_stdout.js
@@ -1,3 +1,4 @@
+/*
 process.stdin.setEncoding('utf8');
 process.stdin.on('data',function(chunk){
     process.stdout.write('chunk:' + chunk.toString());
@@ -5,4 +6,16 @@ process.stdin.on('data',function(chunk){
 process.stdin.on('end',function(){
     process.stdout.write('end');
 })
+*/
 //Usage:  echo 'aaaa' | node stdin_stdout.js
+//
+
+var trans = require('stream').Transform();
+
+trans._transform = function (chunk,encoding,done) {
+    this.push(chunk.toString().split('').map(function(char){
+        return String.fromCharCode(char.charCodeAt(0) + 1);
+    }).join(''));
+    done();
+};
+process.stdin.pipe(trans).pipe(process.stdout);
