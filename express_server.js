bd939a3140c9e3fb340f93a40398790e268e391d
yaowenqiang <yaowenqiang111@163.com>
2015-10-10 00:20:28 +0800

    add some notes

--- express_server.js
+++ express_server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 app.get('',function(req,res){
-    res.send('hello world!');
+    res.send('hello world! how are you?');
 })
 app.listen(3000);
+//useful modules:
+//express,nodemon,mocha,grunt-js,gulp,bower,karma,yo,q
 //browsenpm.org/package.json
+//nodebeginner.org
+//howtonode.org
+//nodeschool.io
