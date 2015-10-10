e4eed486d4a6c2f76b2b6f2eb3d289e55d7fa165
yaowenqiang <yaowenqiang111@163.com>
2015-10-11 02:06:55 +0800

    tide up projects, move routes to a route file

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var routes = function (Book) {
+    var bookRouter = express.Router();
+    bookRouter.route('/books')
+        .post(function(req,res){{
+            var book = new Book(req.body);
+            book.save();
+            //res.send(book);
+            res.status(201).send(book);
+        }})
+        .get(function (req, res) {
+            var query = {};
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            Book.find(query, function (err, books) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(books);
+                }
+            });
+        });
+    bookRouter.route('/books/:bookId')
+        .get(function (req, res) {
+            Book.findById(req.params.bookId, function (err, book) {
+                if (err) {
+                    //                console.log(err);
+                    res.status(500).send(err);
+                } else {
+                    res.json(book);
+                }
+            });
+        });
+    return bookRouter;
+}
+module.exports = routes;
--- app.js
+++ app.js
@@ -8,49 +8,8 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-var bookRouter = express.Router();
 
-
-
-bookRouter.route('/books')
-    .post(function(req,res){{
-        var book = new Book(req.body);
-        //console.log(book);
-        book.save();
-        //res.send(book);
-        res.status(201).send(book);
-    }})
-    .get(function (req, res) {
-        var query = {};
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-        Book.find(query, function (err, books) {
-            if (err) {
-                //                console.log(err);
-                res.status(500).send(err);
-            } else {
-                res.json(books);
-            }
-        });
-        /*
-            var responseJson = {
-                hello: "This is my API"
-            };
-            res.json(responseJson);
-            */
-    });
-bookRouter.route('/books/:bookId')
-    .get(function (req, res) {
-        Book.findById(req.params.bookId, function (err, book) {
-            if (err) {
-                //                console.log(err);
-                res.status(500).send(err);
-            } else {
-                res.json(book);
-            }
-        });
-    });
+bookRouter = require('./Routes/bookRoutes')(Book);
 app.use('/api', bookRouter);
 
 app.get('/', function (req, res) {
