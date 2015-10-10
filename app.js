d73c1da91fc48399583fb9f00bb80f585db634bd
yaowenqiang <yaowenqiang111@163.com>
2015-10-11 01:41:53 +0800

    add body-parse package and get post data from request via body-parse's json method

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://192.168.1.107:27017/bookapi/');
 var Book = require('./models/bookModel');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 var bookRouter = express.Router();
 
 
 
 bookRouter.route('/books')
+    .post(function(req,res){{
+        var book = new Book(req.body);
+        console.log(book);
+        res.send(book);
+    }})
     .get(function (req, res) {
         var query = {};
         if (req.query.genre) {
