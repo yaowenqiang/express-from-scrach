36a6ce76ca8d668d9a7f75f5fb8a8ae5560eba86
yaowenqiang <yaowenqiang111@163.com>
2015-10-11 18:17:57 +0800

    add jade and ejs view

--- express_server.js
+++ express_server.js
@@ -17,6 +17,14 @@ app.get('/',log,function(req,res){
     //res.send('hello world! how are you?');
     res.render('index.jade',{ names:names });
 })
+app.get('/jade',log,function(req,res){
+    //res.send('hello world! how are you?');
+    res.render('jade.jade',{ title:"hello,this is from jade" });
+})
+app.get('/ejs',log,function(req,res){
+    //res.send('hello world! how are you?');
+    res.render('index.ejs',{ title:"hello,this is from ejs" });
+})
 app.post('/',function(req,res){
     names.push(req.body.name);;
     res.redirect('/');
